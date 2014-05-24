9da45f35346e44f4114268ee61228e4bb2f1b9a9
Tobias Sturm <tobstu@gmail.com>
2014-05-24 20:05:20 +0200

    implement screen display

---
+++ screen-multiplexer/screen-display.py
@@ -0,0 +1,20 @@
+import subprocess
+import json
+from messages import event_channel
+
+currentVnc = None
+def startVNC(host):
+    if currentVnc:
+        releaseVNC()
+    currentVnc = sub.Popen(['vncviewer', '-ViewOnly', '-Fullscreen', host],stdout=sub.PIPE,stderr=sub.PIPE, preexec_fn=os.d)
+
+def releaseVNC():
+    if currentVnc:
+        currentVnc.terminate()
+        currentVnc = None
+
+if __name__ == "__main__":
+    channel = event_channel()
+    channel.register_handler("start show display", startVNC)
+    channel.register_handler("stop show display", releaseVNC)
+    channel.loop("ws://141.3.229.49:8080")
