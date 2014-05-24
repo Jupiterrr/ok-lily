a9ef463537df06bb0773b4d5e57445d9cc72751a
Tobias Sturm <tobstu@gmail.com>
2014-05-25 00:30:05 +0200

    tidy up display daemon

--- screenmultiplexer/screen-daemon.py
+++ screenmultiplexer/screen-daemon.py
@@ -4,11 +4,10 @@ import time
 import signal
 import os
 
-def initiate(channel):
-    startVNC()
-    channel.send(channel.generate_message("start show display", channel.my_id, "beamer", channel.get_ip()))
+def initiate():
+    channel.send(channel.generate_message("start show display", "beamer", channel.get_ip()))
 
 if __name__ == "__main__":
     channel = event_channel()
-    channel.register_handler("start share display", lambda payload: initiate(event_channel, payload))
+    channel.register_handler("start share display", initiate)
     channel.loop("ws://192.168.1.8:8080")
