de2eee13751aafd2bcdf219b7886dfc9ac4043e7
Carsten Griesheimer <hallo@carstengriesheimer.de>
2014-05-24 18:48:08 +0200

    Add lights

---
+++ lights/test.js
@@ -0,0 +1,22 @@
+var hue = require("node-hue-api");
+var HueApi = hue.HueApi;
+var lightState = hue.lightState;
+
+var username = "1969998bf08b7ef23d92a9338e01577";
+var id = "001788fffe14f539"
+
+
+// api.getFullState().then(displayResult).done();
+// api.registeredUsers().then(displayResult).done();
+// api.deleteUser("3be8cbb33b7ef70f3d5996ab3dadc687")
+
+// api.lights()
+//   .then(displayResult)
+//   .done();
+
+// api.groups()
+//     .then(displayResult)
+//     .done();
+
+
+
