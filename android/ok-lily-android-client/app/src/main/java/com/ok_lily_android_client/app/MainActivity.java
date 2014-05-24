c852eaa429fb03e5b7cca4bfe37eb8cddf3ebbee
b-sh <b-sh@gmx.net>
2014-05-24 13:19:05 +0200

    Android: add websocket

--- android/ok-lily-android-client/app/src/main/java/com/ok_lily_android_client/app/MainActivity.java
+++ android/ok-lily-android-client/app/src/main/java/com/ok_lily_android_client/app/MainActivity.java
@@ -1,23 +1,33 @@
 package com.ok_lily_android_client.app;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Build;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectWebSocket();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -28,11 +38,54 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://127.0.0.1:8100");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("Websocket", "Message" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    public void sendMessage() {
+        mWebSocketClient.send("Hello");
+    }
 }
