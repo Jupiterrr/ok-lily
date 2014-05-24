49768cb16393fb56b13f64494d89842b6bee2105
Tobias Sturm <tobstu@gmail.com>
2014-05-24 20:02:35 +0200

    move state of screen daemon

--- screen-multiplexer/screen-daemon.py
+++ screen-multiplexer/screen-daemon.py
@@ -4,14 +4,19 @@ import time
 import signal
 import os
 
+currentVnc = None
+
 def startVNC():
-    return subprocess.Popen(['x11vnc','-display', ':0'],stdout=subprocess.PIPE,stderr=subprocess.PIPE, preexec_fn=os.setsid)
+    if currentVnc:
+        stopVNC()
+    currentVnc = subprocess.Popen(['x11vnc','-display', ':0'],stdout=subprocess.PIPE,stderr=subprocess.PIPE, preexec_fn=os.setsid)
 
 def stopVNC(process):
-    os.killpg(process.pid, signal.SIGTERM)
+    if currentVnc:
+        os.killpg(currentVnc.pid, signal.SIGTERM)
 
 if __name__ == "__main__":
-    currentVnc = None
+    vnc = startVNC()
     channel = event_channel()
     channel.register_handler("start share display", startVNC)
     channel.register_handler("stop share display", stopVNC)
