862c0464ac08c34617f11fbbeb626abc27a3baaf
Tobias Sturm <tobstu@gmail.com>
2014-05-24 23:27:42 +0200

    remove stuff we will hack from display daemon

--- screenmultiplexer/screen-daemon.py
+++ screenmultiplexer/screen-daemon.py
@@ -4,15 +4,11 @@ import time
 import signal
 import os
 
-def startVNC():
-    currentVnc = subprocess.Popen(['x11vnc','-display', ':0'],stdout=subprocess.PIPE,stderr=subprocess.PIPE, preexec_fn=os.setsid)
-
-def stopVNC(process):
-    os.killpg(currentVnc.pid, signal.SIGTERM)
+def initiate(channel):
+    startVNC()
+    channel.send(channel.generate_message("start show display", channel.my_id, "beamer", channel.get_ip()))
 
 if __name__ == "__main__":
-    vnc = startVNC()
     channel = event_channel()
-    channel.register_handler("start share display", startVNC)
-    channel.register_handler("stop share display", stopVNC)
+    channel.register_handler("start share display", lambda payload: initiate(event_channel, payload))
     channel.loop("ws://192.168.1.8:8080")
