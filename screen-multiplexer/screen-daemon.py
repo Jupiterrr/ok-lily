50fcca8783813a74b3518d2bf28c8371e0ced80a
Tobias Sturm <tobstu@gmail.com>
2014-05-24 19:06:47 +0200

    make screen-daemon work on linux

--- screen-multiplexer/screen-daemon.py
+++ screen-multiplexer/screen-daemon.py
@@ -1,20 +1,18 @@
 import subprocess
-import json
 from messages import event_channel
+import time
+import signal
+import os
 
 def startVNC():
-    return subprocess.Popen('x11vnc -display :0',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    return subprocess.Popen(['x11vnc','-display', ':0'],stdout=subprocess.PIPE,stderr=subprocess.PIPE, preexec_fn=os.setsid)
 
 def stopVNC(process):
-    process.terminate()
+    os.killpg(process.pid, signal.SIGTERM)
 
 if __name__ == "__main__":
-    vnc = startVNC()
-    time.sleep(60)
-    stopVNC(vnc)
-
-#    currentVnc = None
-#    channel = event_channel()
-#    channel.register_handler("start share display", startVNC)
-#    channel.register_handler("stop share display", stopVNC)
-#    channel.loop("ws://141.3.229.49:8080")
+    currentVnc = None
+    channel = event_channel()
+    channel.register_handler("start share display", startVNC)
+    channel.register_handler("stop share display", stopVNC)
+    channel.loop("ws://141.3.229.49:8080")
